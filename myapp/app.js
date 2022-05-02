da8f00a8a91c83366f4a4101bbac6aabb914fecd
Angelo Dini <angelo.dini@divio.ch>
2022-05-02 08:48:04 +0200

    bumped version to 4.18.0.0

--- myapp/app.js
+++ myapp/app.js
@@ -1,60 +1,62 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 // DIVIO CLOUD ADDITIONS
 if (!process.env.STAGE) {
   // disable caching on localdev
-  app.set('view cache', false);
+  app.set("view cache", false);
 }
 
 // database connection example
-var pgp = require('pg-promise')(/*options*/)
-var db = pgp(process.env.DEFAULT_DATABASE_DSN || 'postgres://postgres@db:5432/db')
+var pgp = require("pg-promise")(/*options*/);
+var db = pgp(
+  process.env.DEFAULT_DATABASE_DSN || "postgres://postgres@db:5432/db"
+);
 
-db.one('SELECT $1 AS value', 123)
+db.one("SELECT $1 AS value", 123)
   .then(function (data) {
-    console.log('DATA:', data.value)
+    console.log("DATA:", data.value);
   })
   .catch(function (error) {
-    console.log('ERROR:', error)
-  })
+    console.log("ERROR:", error);
+  });
 
 module.exports = app;
 
